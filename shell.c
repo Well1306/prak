02e6830fbd067ccb54f0ab1097c850072ce0b376
well1306 <well1306@list.ru>
2020-11-06 00:47:44 +0300

    The 1st shell stage

--- shell.c
+++ shell.c
@@ -5,9 +5,19 @@
 #include <ctype.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <limits.h>
 
 char** mas = NULL;
 char* s = NULL;
+
+void printdir(void) {
+    char* st;
+    char* s;
+    st = malloc(sizeof(char) * PATH_MAX);
+    s = getcwd(st, PATH_MAX);
+    printf("%s$ ", s);
+    free(st);
+}
 
 void delmas(int numb_m) {
     int j;
@@ -17,10 +27,12 @@ void delmas(int numb_m) {
 }
 
 int main() {
+    char* home = getenv("HOME");                                //домашняя директория
     char ch, flag;
     int numb_s = 0, size_s = 0;                             //длина слова 
     int numb_m = 0, size_m = 0;                             //количество слов
     int i;
+    printdir();
     while((ch = getchar()) != EOF) {
         while(ch != '\n') {
             flag = (ch =='\"');
@@ -63,17 +75,23 @@ int main() {
                 delmas(numb_m);
                 exit(0);
             } else {
-                if(!fork()) {
-/*
- *              if(mas[0] == "cd") {
- *                  if(!chdir(
- *              }
- */ 
-                if(execvp(mas[0], mas) == -1) fprintf(stderr, "%s: command not found.\n", mas[0]);
-                break;
-                } else wait(0);
+                if(!strcmp(mas[0],"cd")) {
+                    if(mas[1]) {
+                        if(chdir(mas[1]) == -1) {
+                            printf("%s: %s: No such file or directory.\n", mas[0], mas[1]);
+                        }
+                    } else chdir(home);
+                } else {
+                    if(!fork()) {
+                        if(execvp(mas[0], mas) == -1) {
+                            fprintf(stderr, "%s: command not found\n", mas[0]);
+                            exit(0);
+                        }
+                    } else wait(0);
+                }
             }
         }
+    printdir();
     delmas(numb_m);
     numb_m = size_m = 0;
     }
