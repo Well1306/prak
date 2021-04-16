340db16c8c2b6fed559a418f271a46017b1c43d3
Vladimir Semenov <70581575+Well1306@users.noreply.github.com>
2021-04-16 15:21:24 +0300

    Update client.cpp

--- web server/client.cpp
+++ web server/client.cpp
@@ -5,28 +5,33 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <signal.h>
 #include <vector>
 #include "src/socket.h"
 #include "src/Http.h"
+
+int end = 0;
 
 int main() 
 {
     SocketAddress saddr(1234);
     ConnectedSocket client;
     std::cout << client._connect(saddr) << std::endl;
-    int end = 0;
 
     std::string g;
-    while((g != "exit")) {
-        std::getline(std::cin, g);
+    std::getline(std::cin, g, '%');
+    while((g.find("exit") == std::string::npos) && !end) {
+        int pos = g.find("\n\n");
+        std::cout << g << std::endl;
         client._send(g);
-        if(g == "Close Server") {
+        if(g.find("Close Server") != std::string::npos) {
             std::cout << "Client disconnected." << std::endl;
             end = 1;
             break;
         }
+        std::getline(std::cin, g, '%');
     }
-    if((g != "exit") && !end) {
+    if((g.find("exit") != std::string::npos) || end) {
         g = "exit";
         std::vector<char> buf(g.begin(), g.end());
         client._send(g);
