68ca13ca886d0bef7c004b1d688253e88f1e4f8a
Vladimir Semenov <70581575+Well1306@users.noreply.github.com>
2021-04-22 01:54:27 +0300

    Update client.cpp

--- web server/client.cpp
+++ web server/client.cpp
@@ -10,50 +10,11 @@
 #include "src/socket.h"
 #include "src/Http.h"
 
-void getrequest(std::string &g) { //getline(std::cin, g, "\n\n")
-    g.clear();
-    std::string res;
-    char ch;
-    res.clear();
-    while(1) {
-        getline(std::cin, res);
-        g += res;
-        if((ch = getchar()) == '\n') break;
-        else g += ch;
-        g += "\n";
-    }
-}
-
 int end = 0;
 
 int main() 
 {
-    SocketAddress saddr(1233);
-    ConnectedSocket client;
-    std::cout << client._connect(saddr) << std::endl;
-
-    std::string g,h;
-    HttpResponse result;
-    getrequest(g);
-    while((g.find("exit") == std::string::npos) && !end) {
-        if(!g.empty()) {
-            client._send(g);
-            client._recv(h);
-            std::cout << h << std::endl;
-        }
-        if(g.find("Close Server") != std::string::npos) {
-            std::cout << "Client disconnected." << std::endl;
-            end = 1;
-            break;
-        }
-        getrequest(g);
-        if(g.find("\n") == 0) g.erase(0, 1);
-    }
-    if((g.find("exit") != std::string::npos) || end) {
-        g = "exit";
-        std::vector<char> buf(g.begin(), g.end());
-        client._send(g);
-    }
-
+    ClientSocket client(1233, "Client");
+    client.work();
     return 0;
 }
