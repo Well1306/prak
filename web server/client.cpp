cb16ee1ea2d2393eddbf28c1149a9669ab5db4a2
Vladimir Semenov <70581575+Well1306@users.noreply.github.com>
2021-04-22 00:39:54 +0300

    Update client.cpp

--- web server/client.cpp
+++ web server/client.cpp
@@ -10,6 +10,20 @@
 #include "src/socket.h"
 #include "src/Http.h"
 
+void getrequest(std::string &g) { //getline(std::cin, g, "\n\n")
+    g.clear();
+    std::string res;
+    char ch;
+    res.clear();
+    while(1) {
+        getline(std::cin, res);
+        g += res;
+        if((ch = getchar()) == '\n') break;
+        else g += ch;
+        g += "\n";
+    }
+}
+
 int end = 0;
 
 int main() 
@@ -20,21 +34,19 @@ int main()
 
     std::string g,h;
     HttpResponse result;
-    std::getline(std::cin, g, '%');
+    getrequest(g);
     while((g.find("exit") == std::string::npos) && !end) {
-        // std::cout << g << std::endl;
-        client._send(g);
-        // std::cout << client._recv(h) << std::endl;
-        // result.print();
-        // std::cout << h.empty() << std::endl;
-        client._recv(h);
-        std::cout << h << std::endl;
+        if(!g.empty()) {
+            client._send(g);
+            client._recv(h);
+            std::cout << h << std::endl;
+        }
         if(g.find("Close Server") != std::string::npos) {
             std::cout << "Client disconnected." << std::endl;
             end = 1;
             break;
         }
-        std::getline(std::cin, g, '%');
+        getrequest(g);
         if(g.find("\n") == 0) g.erase(0, 1);
     }
     if((g.find("exit") != std::string::npos) || end) {
