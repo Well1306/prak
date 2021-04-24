ac9e6467a53718c691a430529757a4ff7b746f57
Vladimir Semenov <70581575+Well1306@users.noreply.github.com>
2021-04-24 17:01:23 +0300

    Update Http.cpp

--- web server/src/Http.cpp
+++ web server/src/Http.cpp
@@ -1,5 +1,28 @@
 #include "Http.h"
 
+const std::string GMtime() {
+    struct tm *ptr;
+    time_t a;
+    a = time(NULL);
+    ptr = gmtime(&a);
+    std::string t = asctime(ptr);
+    t = t.substr(0, t.length() - 1);
+    t += " GMT\n";
+    t.insert(3, 1, ',');
+    int pos = t.find("\n");
+    t.erase(pos, 1);
+    return t;
+}
+
+// const int File::length() const {
+//     char c;
+//     int l;
+//     std::cout << 1;
+//     while(read(fd, &c, 1)) l++;
+//     std::cout << 2;
+//     return l;
+// }
+
 URI::URI() {}
 URI::URI(std::string uri) {
     std::string s = uri;
@@ -32,25 +55,11 @@ const headers GetHeader(std::string s) {
     else if(s == "Referer") return REFERER;
     else if(s == "User-agent") return USERAGENT;
     else if(s == "Server") return SERVER;
-    else if(s == "Content-lenght") return CONTENTLENGHT;
+    else if(s == "Content-length") return CONTENTLENGTH;
     else if(s == "Content-type") return CONTENTTYPE;
     else if(s == "Allow") return ALLOW;
     else if(s == "Last-modified") return LASTMODIFIED;
     else return ERROR;
-}
-
-const std::string GMtime() {
-    struct tm *ptr;
-    time_t a;
-    a = time(NULL);
-    ptr = gmtime(&a);
-    std::string t = asctime(ptr);
-    t = t.substr(0, t.length() - 1);
-    t += " GMT\n";
-    t.insert(3, 1, ',');
-    int pos = t.find("\n");
-    t.erase(pos, 1);
-    return t;
 }
 
 HttpHeader::HttpHeader(std::string s) {
@@ -61,7 +70,6 @@ HttpHeader::HttpHeader(std::string s) {
 }
 
 HttpRequest::HttpRequest(std::string request) {
-    // std::cout << "!!!" << request << std::endl;
     std::string s = request;
     int pos = s.find(" ");
     method = s.substr(0, pos);
@@ -84,22 +92,23 @@ HttpRequest::HttpRequest(std::string request) {
         throw p;
     }
 
-    // Основные – Date
-    // Запрос – Host, Referer, User-agent  
-    // Ответ – Server 
-    // Сущности – Content-length, Content-type, Allow, Last-modified
 
-
+    for(int i = 0; i < 8; ++i) 
+        h[i] = 0;
     headers.clear();
     pos = s.find("\n");
     s.erase(0, pos + 1);
     pos = s.find("\n");
     while(pos != (int) s.rfind("\n")) { 
-        headers.push_back(s.substr(0, pos));
+        std::string tmp = s.substr(0, pos);
+        headers.push_back(tmp);
         s.erase(0, pos + 1);
         pos = s.find("\n");
-        // std::cout << pos << ',' << s.rfind("\n") << std::endl;
+        for(int i = 0; i < 8; ++i) {
+            if((int) GetHeader(tmp) == h[i]) h[i] = 1;
+        }
     }
+    // std::cout << h << std::endl;
 }
 void HttpRequest::print() {
     std::cout << "method: " << method << std::endl << "uri: " << http_uri << "prot: " << protocol << std::endl;
@@ -110,62 +119,49 @@ void HttpRequest::print() {
     }
 }
 
-HttpResponse::HttpResponse(HttpRequest& r) {
+HttpResponse::HttpResponse(HttpRequest& r, short p, std::string n, std::string* lm) : port(p), name(n) {
     protocol = "HTTP/" + r.GetProt();
     int error = 0;
-    std::vector<std::string> tmp = r.GetHeaders();
-    while(!tmp.empty()) {
-        std::string t = tmp.front();
-        int pos = t.find(":");
-        std::string head = t.substr(0, pos);
-        std::string value = t.substr(pos + 1);
-        tmp.erase(tmp.begin());
-        switch(GetHeader(head))
-        {
-        case DATE:
-            headers.push_back("Date: " + GMtime());
-            break;
-        case HOST:
-            headers.push_back("Host: 127.0.0.1:1234");
-            break;
-        case REFERER:
-            headers.push_back("Referer: " + r.GetURI());
-            break;
-        case USERAGENT:
-            headers.push_back("User-agent: " + value);
-            break;
-        case SERVER:
-            headers.push_back("Server: WEBSERVER");
-            break;
-        case CONTENTLENGHT:
-            headers.push_back("Content-lenght: " + std::to_string(sizeof(r)));
-            break;
-        case CONTENTTYPE:
-            headers.push_back("Content-type: text/html");
-            break;
-        case ALLOW:
-            headers.push_back("Allow: GET, HEAD");
-            break;
-        case LASTMODIFIED:
-            headers.push_back("Last-modified: " + GMtime());
-            break;
-        default: 
-            error++; 
-            break;
-        }
+    int err404 = 0;
+    file.open(r.GetURI().substr(1).c_str(), std::ios::binary);
+    int f = open(r.GetURI().substr(1).c_str(), O_RDONLY);
+    if(!file) err404 = 1;
+    char c;
+    int len = 0;
+    if(f != - 1) {
+        while(read(f, &c, 1))
+            len++;
+        close(f);
     }
+    std::string ftype;
+    if(r.GetURI().substr(1).find("jpg") != std::string::npos) ftype = "image/jpeg";
+    else if (r.GetURI().substr(1).find("png") != std::string::npos) ftype = "image/apng";
+    else ftype = "text/html";
+    headers.push_back("Date: " + GMtime());
+    headers.push_back("Host: 127.0.0.1:" + std::to_string(port));
+    headers.push_back("Server: " + name);
+    headers.push_back("Referer: " + r.GetURI());
+    headers.push_back("Content-length: " + std::to_string(len));
+    headers.push_back("Content-type: " + ftype);
+    headers.push_back("Allow: GET, HEAD");
+    headers.push_back("Last-modified: " + (*lm));
+    *lm = GMtime();
     if(error != 0) {
-        code = 404;
-        exp = "Not Found";
-        headers.clear();
-    } else { code = 200; exp = "OK"; }
+        code = 501;
+        exp = "Not Implemented";
+        // headers.clear();
+    } else {
+        if(err404 != 0) { code = 404; exp = "Not Found"; }
+        else { code = 200; exp = "OK"; }
+    }
 }
 
 const std::string HttpResponse::to_string() const {
-    std::string res = protocol + " " + std::to_string(code) + " " + exp + "\n";
+    std::string res = protocol + " " + std::to_string(code) + " " + exp + "\n\r";
     std::vector<std::string> tmp = headers;
+    // std::cout << tmp.empty() << std::endl;
     while(!tmp.empty()) {
-        res += tmp.front() + "\n";
+        res += tmp.front() + "\n\r";
         tmp.erase(tmp.begin());
     }
     return res;
