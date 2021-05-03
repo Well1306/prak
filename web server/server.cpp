bc5153f5d10a2407b3161e28dfc86014a82cf899
Vladimir Semenov <70581575+Well1306@users.noreply.github.com>
2021-05-03 23:16:21 +0300

    Update server.cpp

--- web server/server.cpp
+++ web server/server.cpp
@@ -6,16 +6,16 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <fcntl.h>
 #include <vector>
 #include "src/socket.h"
 #include "src/Http.h"
 
-// GET cgi-bin/testcgi?name=igor&surname=golovin&mail=igolovin HTTP/1.1
-// GET /index.html HTTP/1.1
 
 int main()
 {
-    ServerSocket server(1233, "MyServer", "log.txt");
+    std::cout << 1 << std::endl;
+    ServerSocket server(1234, "MyServer", "log.txt");
     server.work();
     return 0;
 }
