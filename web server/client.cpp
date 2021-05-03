4e5f8f5a578a08e2f38c48de7f383cc33d55c915
Vladimir Semenov <70581575+Well1306@users.noreply.github.com>
2021-05-03 23:15:36 +0300

    Update client.cpp

--- web server/client.cpp
+++ web server/client.cpp
@@ -6,15 +6,19 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <signal.h>
+#include <fcntl.h>
 #include <vector>
 #include "src/socket.h"
 #include "src/Http.h"
+
+// GET cgi-bin/testcgi?name=igor&surname=golovin&mail=igolovin HTTP/1.1
+// GET /index.html HTTP/1.1
 
 int end = 0;
 
 int main() 
 {
-    ClientSocket client(1233, "Client");
+    ClientSocket client(1234, "Client");
     client.work();
     return 0;
 }
