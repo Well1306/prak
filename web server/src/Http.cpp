78982161c6cb1b52337efde692848b77be66be01
Vladimir Semenov <70581575+Well1306@users.noreply.github.com>
2021-05-04 19:11:38 +0300

    Update Http.cpp

--- web server/src/Http.cpp
+++ web server/src/Http.cpp
@@ -1,4 +1,5 @@
 #include "Http.h"
+#include "socket.h"
 #include <sys/wait.h>
 #include <errno.h>
 
@@ -16,15 +17,6 @@ const std::string GMtime() {
     t.erase(pos, 1);
     return t;
 }
-
-// const int File::length() const {
-//     char c;
-//     int l;
-//     std::cout << 1;
-//     while(read(fd, &c, 1)) l++;
-//     std::cout << 2;
-//     return l;
-// }
 
 URI::URI() {}
 URI::URI(std::string uri) {
@@ -47,33 +39,42 @@ const std::string URI::GetFile() const {
     std::string s = way.substr(1, way.length());
     return s;
 }
-void URI::_cgi(int* err) {
+char* URI::_cgi(int* err, int* len) {
     int fd = open("t.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
     int status;
     pid_t pid = fork();
     if(pid > 0) {
         wait(&status);
-        if(WIFEXITED(status) && WEXITSTATUS(status) == 0) *err = 0;
+        if(WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            *err = 0;
+            close(fd);
+            *len = 0;
+            std::ifstream f;
+            f.open("t.txt");
+            char* c = to_str(f, *len);
+            return c;
+        }
         else *err = 1;
     }
     else if(pid == 0) {
         char* argv[] = { (char*) GetFile().c_str(), NULL };
         std::vector<std::string> tmp = GetPar();
-        char** envp = new char*[tmp.size() + 1];
-        int i = 0;
         while(!tmp.empty()) {
             std::string s = tmp.front();
-            envp[i] = strdup(s.c_str());
-            i++;
+            size_t pos = s.find('=');
+            std::string s1 = s.substr(0, pos);
+            s.erase(0, pos + 1);
+            // std::cout << s1 << " = " << s << std::endl;
+            setenv(s1.c_str(), s.c_str(), 0);
             tmp.erase(tmp.begin());
         }
-        envp[i] = NULL;
         dup2(fd, 1);
         close(fd);
-        execve((char*) GetFile().c_str(), argv, envp);
+        execv((char*) GetFile().c_str(), argv);
         perror("11");
         exit(1);
     }
+    return 0;
 }
 
 const headers GetHeader(std::string s) {
@@ -129,7 +130,6 @@ HttpRequest::HttpRequest(std::string request) {
         s.erase(0, pos + 1);
         pos = s.find("\n");
     }
-    // std::cout << h << std::endl;
 }
 void HttpRequest::print() {
     std::cout << "method: " << method << std::endl << "uri: " << http_uri << "prot: " << protocol << std::endl;
@@ -145,15 +145,12 @@ HttpResponse::HttpResponse(HttpRequest& r, short p, std::string n, std::string* 
     int error = 0;
     int err404 = 0;
     file.open(r.GetURI().substr(1).c_str(), std::ios::binary);
-    int f = open(r.GetURI().substr(1).c_str(), O_RDONLY);
     if(!file) err404 = 1;
-    char c;
-    int len = 0;
-    if(f != - 1) {
-        while(read(f, &c, 1))
-            len++;
-        close(f);
+    if(r.IsCGI() == 1) {
+        body = r._GetURI()._cgi(&err404, &content_length);
+        // std::cout << "&&&" << body << std::endl;
     }
+    else body = to_str(file, content_length);
     std::string ftype;
     if(r.GetURI().substr(1).find("jpg") != std::string::npos) ftype = "image/jpeg";
     else if (r.GetURI().substr(1).find("png") != std::string::npos) ftype = "image/apng";
@@ -162,16 +159,14 @@ HttpResponse::HttpResponse(HttpRequest& r, short p, std::string n, std::string* 
     headers.push_back("Host: 127.0.0.1:" + std::to_string(port));
     headers.push_back("Server: " + name);
     headers.push_back("Referer: " + r.GetURI());
-    headers.push_back("Content-length: " + std::to_string(len));
+    headers.push_back("Content-length: " + std::to_string(content_length));
     headers.push_back("Content-type: " + ftype);
     headers.push_back("Allow: GET, HEAD");
     headers.push_back("Last-modified: " + (*lm));
     *lm = GMtime();
-    if(r.IsCGI() == 1) r._GetURI()._cgi(&err404);
     if(error != 0) {
         code = 501;
         exp = "Not Implemented";
-        // headers.clear();
     } else {
         if(err404 != 0) { code = 404; exp = "Not Found"; }
         else { code = 200; exp = "OK"; }
@@ -181,7 +176,6 @@ HttpResponse::HttpResponse(HttpRequest& r, short p, std::string n, std::string* 
 const std::string HttpResponse::to_string() const {
     std::string res = protocol + " " + std::to_string(code) + " " + exp + "\n\r";
     std::vector<std::string> tmp = headers;
-    // std::cout << tmp.empty() << std::endl;
     while(!tmp.empty()) {
         res += tmp.front() + "\n\r";
         tmp.erase(tmp.begin());
@@ -191,10 +185,10 @@ const std::string HttpResponse::to_string() const {
 
 
 void HttpResponse::print() {
-        std::cout << protocol << " " << std::to_string(code) << " " << exp << std::endl;
-        std::vector<std::string> tmp = headers;
-        while(!tmp.empty()) {
-            std::cout << tmp.front() << std::endl;
-            tmp.erase(tmp.begin());
-        }
+    std::cout << protocol << " " << std::to_string(code) << " " << exp << std::endl;
+    std::vector<std::string> tmp = headers;
+    while(!tmp.empty()) {
+        std::cout << tmp.front() << std::endl;
+        tmp.erase(tmp.begin());
     }
+}
